7d00d7fa29274bec7b97d1b1ad7b24798838814a
Melvin Tu Macbook <melvin@meldano.com>
2025-10-28 15:21:49 +1100

    Testing directories

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,20 +2,33 @@ import os
 
 def get_files_info(working_directory, directory="."):
     
-    full_path = os.path.join(working_directory, directory)
-    
-    print(full_path)
-    if os.path.isdir(full_path):
-        print("not a directory")
+    #joined_path = os.path.join(working_directory, directory)
+
+    if directory.startswith("/"):
+        removed_slash = directory.replace("/", "")
+        joined_path = os.path.join(working_directory, removed_slash)
+    else:
+        added_slash = f"/{directory}"
+        joined_path = os.path.join(working_directory, added_slash)
+        #print(joined_path)
+
+    #print(full_path, "this is the full path")
+    if os.path.isdir(joined_path):
+        print("not a directory", joined_path)
         return f'Error: "{directory}" is not a directory'
+    else:
+        print(joined_path, "is a directory")
     
-    if os.path.abspath(full_path) != working_directory:
-        print("outside working directory")
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
     
-    items = os.listdir(directory) #creates a list of each item in the directory
-    contents = {}
-    for item in items:
-        if os.path.isfile(item):
-            print(True)
-            return
+   # if working_directory not in os.path.abspath(joined_path):
+        #print("outside working directory", os.path.abspath(joined_path))
+        #return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+
+    #print(True, joined_path)
+    #items = os.listdir(directory) #creates a list of each item in the directory
+    #contents = {}
+    #for item in items:
+     #   if os.path.isfile(item):
+      #      print(True)
+       #     return
