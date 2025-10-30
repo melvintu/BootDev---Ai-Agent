ccfc23d61e50d6fd7209c11fd251e82ae0aa4ed9
melvintu <melvin@meldano.com>
2025-10-30 22:56:17 +1100

    Added in function to run python files inside working directory

---
+++ functions/run_python_file.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(abs_file_path):
+        return f'Error: File "{file_path}" not found.'
+    if abs_file_path[-3:] != ".py":
+        return f'Error: "{file_path}" is not a Python file.'
+    commands = ["python", abs_file_path]
+    try: 
+        if args:
+            commands.extend(args)
+        completed_process = subprocess.run(commands,
+                                           timeout=30, 
+                                           capture_output=True,
+                                           text=True,
+                                           cwd=abs_working_dir
+                                           )
+        output = []
+        if completed_process.stdout:
+            output.append(f"STDOUT:\n{completed_process.stdout}")
+        if completed_process.stderr:
+            output.append(f"STDERR:\n{completed_process.stderr}")
+        if completed_process.returncode != 0:
+            output.append(f"Process exited with code {completed_process.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+    except Exception as e:
+        return f"Error: executing Python file: {e}" 
--- tests.py
+++ tests.py
@@ -1,19 +1,32 @@
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 
 def test():
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    result = run_python_file("calculator", "main.py")
     print(result)
     print("")
 
-    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    result = run_python_file("calculator", "main.py", ["3 + 5"])
     print(result)
     print("")
 
-    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
-    print(result)
+    tests = run_python_file("calculator", "tests.py")
+    print(tests)
+    print("")
 
+    result = run_python_file("calculator", "../main.py")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "nonexistent.py")
+    print(result)
+    print("")
+
+    result = run_python_file("calculator", "lorem.txt")
+    print(result)
+    print("")
 
 if __name__ == "__main__":
     test()
