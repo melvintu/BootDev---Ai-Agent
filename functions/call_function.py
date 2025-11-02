fc422670f31aa5761ea7ab963e76c82c4afe6825
melvintu <melvin@meldano.com>
2025-11-02 22:01:13 +1100

    Added in function calling for AI, can return results in --verbose as well or just a clean output

--- functions/call_function.py
+++ functions/call_function.py
@@ -1,9 +1,8 @@
 from google.genai import types
-
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = types.Tool(
     function_declarations=[
@@ -14,6 +13,41 @@ available_functions = types.Tool(
     ]
 )
 
-
 def call_function(function_call_part, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else: 
+        print(f" - Calling function: {function_call_part.name}")
+
+    function_map = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file
+    }
+
+    if function_call_part.name in function_map:
+        func = function_map[function_call_part.name]
+        args = dict(function_call_part.args)
+        args["working_directory"] = "./calculator"
+        result = func(**args)
+
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"result": result},
+                )
+            ],
+        )
     
+    return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.call_function import available_functions
+from functions.call_function import call_function
 from prompts import system_prompt
 
 def main():
@@ -51,7 +52,15 @@ def generate_content(client, messages, verbose):
         return response.text
 
     for function_call_part in response.function_calls:
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        function_call_result = call_function(function_call_part, verbose) 
+        try:
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+            else: 
+                print(function_call_result.parts[0].function_response.response["result"])
+        except Exception as e:
+            print(f"Error calling function: {e}")
+        
 
 if __name__ == "__main__":
     main()
